a59304c18a90176c94f16345b5fd12e9331e7d84
Omari Wallace <omari@qubit.com>
2017-01-19 16:40:29 -0500

    added pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'tutorial-2',
+    script: './index.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-54-152-49-16.compute-1.amazonaws.com',
+      key: '~/.ssh/AWS.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:omariwallace/simple-server.git',
+      path: '/home/ubuntu/simple-server',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
